2d95d22b8d4d466261cce9cea0191d69e0461356
Rakibul Islam <rakibul.rir06@gmail.com>
2025-04-02 18:08:10 -0600

    Add files via upload

---
+++ examples/led_control/led_control.ino
@@ -0,0 +1,39 @@
+/**
+ * WebController LED Control Example
+ * 
+ * This example demonstrates how to use the WebController library 
+ * to control an LED using a button from the WebSocket-based controller.
+ * 
+ * Components:
+ * - ESP32
+ * - LED connected to pin 2
+ * 
+ * Author: Rakibul Islam
+ * License: CC BY-NC 4.0
+ */
+
+#include "web_controller.h"
+
+#define LED_PIN 2
+
+const char* ssid = "BDOSN4";       // Your WiFi SSID
+const char* password = "Bdosn2023@"; // Your WiFi Password
+
+// Create the WebController object with WiFi credentials
+WebController webController(ssid, password);
+
+void setup() {
+    Serial.begin(115200);
+    webController.begin(); // Initialize the WebController
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    webController.loop(); // Handle WebSocket communication
+    
+    // Get the status of button 0 from the web controller
+    bool buttonState = webController.getButtonState(0);
+
+    // Control the LED based on button state
+    digitalWrite(LED_PIN, buttonState ? HIGH : LOW);
+}
